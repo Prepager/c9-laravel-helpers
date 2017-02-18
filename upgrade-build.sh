3c922748cade63db2308790315bfad7e5a7db3b2
Andreas <thecodingbeast@gmail.com>
2017-02-18 02:28:51 +0100

    Force yes on upgrade

--- upgrade-build.sh
+++ upgrade-build.sh
@@ -26,7 +26,7 @@ function upgradingMySQL {
     wget http://dev.mysql.com/get/mysql-apt-config_0.7.3-1_all.deb
     sudo dpkg -i mysql-apt-config_0.7.3-1_all.deb
     sudo apt-get update
-    sudo apt-get install mysql-server -y
+    sudo apt-get install mysql-server -y --force-yes
     rm mysql-apt-config_0.7.3-1_all.deb
 }
 upgradingMySQL
